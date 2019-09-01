455190d7ff9ce66d8e78de54c68858cd52248ee4
ExoMemphiz <ExoMemphiz@gmail.com>
2019-09-01 10:43:54 +0200

    Made async function & deleted types.ts

--- app.ts
+++ app.ts
@@ -3,61 +3,62 @@ import * as fs from "fs";
 import * as sharp from "sharp";
 import { CreateXML } from "./helpers/FileHelper";
 
-const wcDir = `${os.homedir}\\Documents\\WorldCreator\\Library\\Textures\\Ground\\WorldCreator`;
-const quixelLib = `${os.homedir}\\Documents\\Megascans Library\\Downloaded\\surface`;
+const args = process.argv;
+
+const wcDir = args[2] || `${os.homedir}\\Documents\\WorldCreator\\Library\\Textures\\Ground\\WorldCreator`;
+const quixelLib = args[3] || `${os.homedir}\\Documents\\Megascans Library\\Downloaded\\surface`;
 const quixelFiles = new Map<string, Array<string>>();
+
 (() => {
     try {
         process.stdout.write("Reading World Creator and Quixel Library folders...");
         fs.readdirSync(quixelLib).forEach((dirName) => {
             quixelFiles.set(dirName, fs.readdirSync(`${quixelLib}\\${dirName}`));
         });
-
         quixelFiles.forEach((values, dirName) => {
             const albedo = values.filter((file) => file.indexOf("Albedo") > 0)[0];
             const normal = values.filter((file) => file.indexOf("Normal") > 0)[0];
             const displacement = values.filter(
                 (file) => file.indexOf("Displacement.jpg") > 0 || file.indexOf("Displacement.png") > 0
             )[0];
-            if (!albedo || !normal || !displacement) {
-                return;
+            if (!albedo) {
+                throw Error(`No "Albedo" file could be found.`);
+            } else if (!normal) {
+                throw Error(`No "Normal" file could be found.`);
+            } else if (!displacement) {
+                throw Error(`No "Displacement" file could be found.`);
             }
             values.forEach(async (element) => {
                 console.log(`Creating folder for: ${dirName}`);
                 if (!fs.existsSync(`${wcDir}\\${dirName}`)) {
-                    setTimeout(() => {}, 5000);
                     fs.mkdirSync(`${wcDir}\\${dirName}`);
-                    const albedoThumb = albedo ? albedo.split(".") : null;
-                    const normalThumb = normal ? normal.split(".") : null;
-                    const displacementThumb = displacement ? displacement.split(".") : null;
-                    if (albedoThumb && normalThumb && displacementThumb)
-                        if (albedo) {
-                            fs.copyFileSync(`${quixelLib}\\${dirName}\\${albedo}`, `${wcDir}\\${dirName}\\${albedo}`);
-                            await sharp(`${quixelLib}\\${dirName}\\${albedo}`)
-                                .resize(128, 128)
-                                .toFile(`${wcDir}\\${dirName}\\${albedoThumb[0]}_thumb.${albedoThumb[1]}`);
-                        }
-                    if (normal) {
-                        fs.copyFileSync(`${quixelLib}\\${dirName}\\${normal}`, `${wcDir}\\${dirName}\\${normal}`);
-                        await sharp(`${quixelLib}\\${dirName}\\${albedo}`)
-                            .resize(128, 128)
-                            .toFile(`${wcDir}\\${dirName}\\${normalThumb[0]}_thumb.${normalThumb[1]}`);
-                    }
-                    if (displacement) {
-                        fs.copyFileSync(
-                            `${quixelLib}\\${dirName}\\${displacement}`,
-                            `${wcDir}\\${dirName}\\${displacement}`
-                        );
-                        await sharp(`${quixelLib}\\${dirName}\\${albedo}`)
-                            .resize(128, 128)
-                            .toFile(`${wcDir}\\${dirName}\\${displacementThumb[0]}_thumb.${displacementThumb[1]}`);
-                    }
+                    const albedoThumb = albedo.split(".");
+                    const normalThumb = normal.split(".");
+                    const displacementThumb = displacement.split(".");
+                    
+                    await copySync(dirName, albedo, albedoThumb);
+                    await copySync(dirName, normal, normalThumb);
+                    await copySync(dirName, displacement, displacementThumb);
+
+                    await sharp(`${quixelLib}\\${dirName}\\${albedo}`)
+                        .resize(128, 128)
+                        .toFile(`${wcDir}\\${dirName}\\${displacementThumb[0]}_thumb.${displacementThumb[1]}`);
+
                     fs.writeFileSync(
                         `${wcDir}\\${dirName}\\Description.xml`,
-                        await CreateXML(`${albedoThumb[0]}_thumb.${albedoThumb[1]}`, albedo, normal, displacement)
+                        await CreateXML(`${albedoThumb[0]}_thumb.${albedoThumb[1]}`, albedo, normal, displacement),
                     );
                 }
             });
         });
-    } catch (err) {}
+    } catch (err) {
+        throw err;
+    }
 })();
+
+async function copySync(dirName: string, dirFile: string, dirThumb: Array<string>) {
+    fs.copyFileSync(`${quixelLib}\\${dirName}\\${dirFile}`, `${wcDir}\\${dirName}\\${dirFile}`);
+    await sharp(`${quixelLib}\\${dirName}\\${dirFile}`)
+        .resize(128, 128)
+        .toFile(`${wcDir}\\${dirName}\\${dirThumb[0]}_thumb.${dirThumb[1]}`);
+}
