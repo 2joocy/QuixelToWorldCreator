9a9538ed57d289dd6dba5d75aff6c070dbbd6e70
William Pfaffe <william@hedia.co>
2019-10-07 13:13:49 +0200

    Added checks for World Creator folder

--- app.ts
+++ app.ts
@@ -5,22 +5,28 @@ import { CreateXML, dirExists } from "./helpers/FileHelper";
 
 const args = process.argv;
 
-const wcDir = args[2] || `${os.homedir()}\\Documents\\WorldCreator\\Library\\Textures\\Ground\\WorldCreator`;
+const wcDir = args[2] || `${os.homedir()}/DocumentWorldCreator/Library/Textures/Ground/WorldCreator`;
 const quixelLib = args[3] || `${os.homedir()}/Documents/Megascans Library/Downloaded/surface`;
 const quixelFiles = new Map<string, Array<string>>();
 
 (() => {
     try {
-        if(!dirExists(quixelLib)){
+        if(!fs.existsSync(quixelLib)){
             console.error(`There were no textures found under \n${quixelLib} \nEither set it manually, or contact me on discord at William Pfaffe#9520`)
             return;
         }
+
+        if(!fs.existsSync(wcDir)){
+            console.error(`No World Creator directory found under \n${wcDir} \nEither set it manually, or contact me on discord at William Pfaffe#9520`)
+            return;
+        }
+
         if(fs.readdirSync(quixelLib).length < 1){
             console.error("No Quixel Textures found!");
         }
 
         fs.readdirSync(quixelLib).forEach((dirName) => {
-            quixelFiles.set(dirName, fs.readdirSync(`${quixelLib}\\${dirName}`));
+            quixelFiles.set(dirName, fs.readdirSync(`${quixelLib}/${dirName}`));
         });
         process.stdout.write("Reading World Creator and Quixel Library folders...");
         quixelFiles.forEach((values, dirName) => {
@@ -32,10 +38,13 @@ const quixelFiles = new Map<string, Array<string>>();
             if (!albedo || !normal || !displacement) {
                 return;
             }
+            if(values.length < 1){
+                console.log(`No textures found at ${dirName}`)
+            }
             values.forEach(async () => {
-                if (!fs.existsSync(`${wcDir}\\${dirName}`)) {
+                if (!fs.existsSync(`${wcDir}/${dirName}`)) {
                     console.log(`Creating folder for: ${dirName}`);
-                    fs.mkdirSync(`${wcDir}\\${dirName}`);
+                    fs.mkdirSync(`${wcDir}/${dirName}`);
                     const albedoThumb = albedo.split(".");
                     const normalThumb = normal.split(".");
                     const displacementThumb = displacement.split(".");
@@ -44,24 +53,25 @@ const quixelFiles = new Map<string, Array<string>>();
                     await copySync(dirName, normal, normalThumb);
                     await copySync(dirName, displacement, displacementThumb);
 
-                    await sharp(`${quixelLib}\\${dirName}\\${albedo}`)
+                    await sharp(`${quixelLib}/${dirName}/${albedo}`)
                         .resize(128, 128)
-                        .toFile(`${wcDir}\\${dirName}\\${displacementThumb[0]}_thumb.${displacementThumb[1]}`);
+                        .toFile(`${wcDir}/${dirName}/${displacementThumb[0]}_thumb.${displacementThumb[1]}`);
 
                     fs.writeFileSync(
-                        `${wcDir}\\${dirName}\\Description.xml`,
+                        `${wcDir}/${dirName}/Description.xml`,
                         await CreateXML(`${albedoThumb[0]}_thumb.${albedoThumb[1]}`, albedo, normal, displacement)
                     );
                 }
             });
         });
     } catch (err) {
+        console.log(err);
     }
 })();
 
 async function copySync(dirName: string, dirFile: string, dirThumb: Array<string>) {
-    fs.copyFileSync(`${quixelLib}\\${dirName}\\${dirFile}`, `${wcDir}\\${dirName}\\${dirFile}`);
-    await sharp(`${quixelLib}\\${dirName}\\${dirFile}`)
+    fs.copyFileSync(`${quixelLib}/${dirName}/${dirFile}`, `${wcDir}/${dirName}/${dirFile}`);
+    await sharp(`${quixelLib}/${dirName}/${dirFile}`)
         .resize(128, 128)
-        .toFile(`${wcDir}\\${dirName}\\${dirThumb[0]}_thumb.${dirThumb[1]}`);
+        .toFile(`${wcDir}/${dirName}/${dirThumb[0]}_thumb.${dirThumb[1]}`);
 }
